8dc806f0b42c72d094b82b0b97a2de3d97d294a1
razielar <razielar@gmail.com>
2020-02-12 18:53:11 +0100

    add complete plot

--- mutate_examples/mutate.at.vars.replace.R
+++ mutate_examples/mutate.at.vars.replace.R
@@ -47,53 +47,67 @@ input %<>% filter(!(is.na(L3_mRNA) & is.na(mRNA_Regeneration.25h))) %>%
     mutate_at(vars(contains("Regeneration")), ~replace(., . < 1, 0)) 
 
 
-tmp <- melt(input[1,])
-tmp$variable <- as.character(tmp$variable)
-tmp$variable[c(2:7,9:14)] %<>% 
-    strsplit(., split=".", fixed=TRUE) %>% lapply(., function(x){y <- x[2]}) %>%
-    unlist
-tmp$variable[8] <- "L3"
-tmp$variable <- as.factor(tmp$variable)
-tmp$variable <- factor(tmp$variable,
-       levels = levels(tmp$variable)[c(4,1:3)])
+## Quality control to see if we grab what we want to 
+## input %>% filter(!(is.na(L3_mRNA) & is.na(mRNA_Regeneration.25h))) %>%
+##     select_at(vars(contains("Regeneration")))
 
-### Not reproducible
+######### Plot first example:
 
-tmp %<>% select(Gene_Name, partnerRNA_gene, variable, value) %>%
-    add_row(., Gene_Name="lncRNA:let7C", partnerRNA_gene="CG10283",
-            variable="L3", value=2.01, .after = 4) %>%
-    add_row(.,Gene_Name="lncRNA:let7C", partnerRNA_gene="CG10283",
-            variable="L3", value=2.31, .after = 12)
+## pdf(file = "Plots//association.lncRNA.mRNA.pdf", paper = "a4r", width = 0, height = 0)
 
-### Not reproducible
+for(i in 1:nrow(input)){
 
-tmp$Treatment <- rep(c(rep("Control", 4), rep("Regeneration",4)),2)
+    cat(i, "\n")
+    
+    tmp <- melt(input[i,])
+    tmp$variable <- as.character(tmp$variable)
+    tmp$variable[c(2:7,9:14)] %<>% 
+        strsplit(., split=".", fixed=TRUE) %>% lapply(., function(x){y <- x[2]}) %>%
+        unlist
+    tmp$variable[8] <- "L3"
+    tmp$variable <- as.factor(tmp$variable)
+    tmp$variable <- factor(tmp$variable,
+                           levels = levels(tmp$variable)[c(4,1:3)])
 
-tmp[c(9:16),1] <- rep(tmp[1,2],8)
-tmp$partnerRNA_gene <- NULL
+    tmp <- tmp %>% select(Gene_Name, partnerRNA_gene, variable, value)
 
-### Not reproducible
+    #Gene_Name, partnerRNA_gene and their values: 
+    gene_name <- tmp[1,1]
+    partner <- tmp[1,2]
+    gene_value <- tmp[1,4]
+    partner_value <- tmp[8,4]
+    
+    tmp <- tmp %>% add_row(., Gene_Name=gene_name, partnerRNA_gene=partner,
+                           variable="L3", value=gene_value, .after = 4) %>%
+        add_row(.,Gene_Name=gene_name, partnerRNA_gene=partner,
+                variable="L3", value=partner_value, .after = 12)
 
-tmp$Gene_Name <- as.factor(tmp$Gene_Name)
+    tmp$Treatment <- rep(c(rep("Control", 4), rep("Regeneration",4)),2)
 
-tmp$Gene_Name <- factor(tmp$Gene_Name,
-       levels = levels(tmp$Gene_Name)[c(2,1)])
+    tmp[c(9:16),1] <- rep(tmp[1,2],8)
+    tmp$partnerRNA_gene <- NULL
+    
+    #lncRNA always goes to the left:
+    tmp$Gene_Name <- as.factor(tmp$Gene_Name)
+    new_order <- match(as.character(unique(tmp$Gene_Name)), levels(tmp$Gene_Name))
+    tmp$Gene_Name <- factor(tmp$Gene_Name,
+                            levels = levels(tmp$Gene_Name)[ new_order ])
 
-### Not reproducible
 
-## pdf(file = "Plots//association.lncRNA.mRNA.tmp.pdf", paper = "a4r",
-##     width = 0, height = 0)
+    p <- ggplot(data = tmp, aes(x=variable, y=value, group=Treatment))+
+        geom_line( aes(linetype=Treatment, color=Treatment))+
+        geom_point(aes(color=Treatment))+facet_wrap(~Gene_Name, scales = "free")+
+        xlab("")+ylab("TPMs")+theme_light()+
+        theme(legend.position = "bottom", strip.text = element_text(size=11))+
+        labs(color="")+
+        labs(linetype="")+
+        scale_color_manual(values = c(control_sample, regene_sample))
 
-ggplot(data = tmp, aes(x=variable, y=value, group=Treatment))+
-    geom_line( aes(linetype=Treatment, color=Treatment))+
-    geom_point(aes(color=Treatment))+facet_wrap(~Gene_Name, scales = "free")+
-    xlab("")+ylab("TPMs")+theme_light()+
-    theme(legend.position = "bottom", strip.text = element_text(size=11))+
-    labs(color="")+
-    labs(linetype="")+
-    scale_color_manual(values = c(control_sample, regene_sample))
+    plot(p)
 
+}
 dev.off()
+
 
 
 
