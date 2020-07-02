96d6053935758051c8a4d09ebbb7f40b61d830da
razielar <razielar@gmail.com>
2020-07-02 18:32:19 +0200

    write the 5 different methods

--- benchmarks/benchmark.reading.comparison.R
+++ benchmarks/benchmark.reading.comparison.R
@@ -1,13 +1,31 @@
-### Benchmark different methods to read files:
+### Benchmark 5 different methods to read files:
 
 library(tidyverse)
 ## install.packages("microbenchmark")
 library(microbenchmark)
-
-
-
+library(feather)
+library(data.table)
 options(stringsAsFactors = F)
 setwd('/home/razielar/Documents/git_rep/Dplyr-examples/')
+
+### csv example:
+## example <- read_csv("benchmarks/awards.csv")
+
+
+## path_feather <- "benchmarks/awards.feather"
+## path_rdata <- "benchmarks/awards.RData"
+## path_rds <- "benchmarks/awards.rds"
+
+### Write the example csv:
+
+## write_feather(example, path = path_feather)
+## save(example, file=path_rdata)
+## saveRDS(example, path_rds)
+
+
+
+
+
 
 
 
