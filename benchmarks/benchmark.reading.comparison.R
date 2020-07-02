e8d4e7f787d700a059b43710406fec8fa557a588
razielar <razielar@gmail.com>
2020-07-02 18:19:56 +0200

    create benchmark folder

---
+++ benchmarks/benchmark.reading.comparison.R
@@ -0,0 +1,17 @@
+### Benchmark different methods to read files:
+
+library(tidyverse)
+## install.packages("microbenchmark")
+library(microbenchmark)
+
+
+
+options(stringsAsFactors = F)
+setwd('/home/razielar/Documents/git_rep/Dplyr-examples/')
+
+
+
+
+
+
+
